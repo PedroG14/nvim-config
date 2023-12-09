0e4ae2e18ba2475653c2f66022cbce79472f4258
PedroG14 <leo140201@hotmail.com>
2023-12-09 14:06:45 -0300

    Updated treesitter-textobjects settings

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -23,7 +23,37 @@ return {
             auto_install = true,
             highlight = { enable = true },
             indent = { enable = true },
-            autotag = { enable = true }
+
+            -- Autotag
+            autotag = { enable = true },
+
+            -- Textobjects
+            textobjects = {
+                select = {
+                    enable = true,
+                    lookahead = true,
+                    keymaps = {
+                        ['af'] = '@function.outer',
+                        ['if'] = '@function.inner',
+                        ['ac'] = '@class.outer',
+                        ['ic'] = {
+                            query = '@class.inner',
+                            desc = 'Select inner part of a class region'
+                        },
+                        ['as'] = {
+                            query = '@scope',
+                            query_group = 'locals',
+                            desc = 'Select language scope'
+                        }
+                    },
+                    selection_modes = {
+                        ['@parameter.outer'] = 'v', -- charwise
+                        ['@function.outer'] = 'V', -- linewise
+                        ['@class.outer'] = '<c-v>', -- blockwise
+                    },
+                    include_surrounding_whitespace = true
+                }
+            }
         }
 
         treesitter.setup(opts)
